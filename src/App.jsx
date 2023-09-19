001b4cc7288ab2dd23a3400af767f907c6195b95
NZ <n.prokopyk@gmail.com>
2023-09-19 11:02:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        <a href="" target="blank">
+          Git Hub
+        </a>
+      </footer>
+    </div>
+  );
+}
